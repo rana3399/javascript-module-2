611724bc154c2cd6185eee486fdd2360a5323fd6
Rana Ahmed <foster3399@gmail.com>
2021-07-30 21:07:11 +0200

    Module -2 Exercise

--- week-1/Homework/dom-manipulation-1/exercise.js
+++ week-1/Homework/dom-manipulation-1/exercise.js
@@ -16,18 +16,18 @@ Write JavaScript below that logs:
 
 */
 
-// let p_tagName = document.getElementsByTagName('p');
-// console.log(p_tagName);
+let p_tagName = document.getElementsByTagName('p');
+console.log(p_tagName);
 
-// let firstDivElement = document.querySelector('div');
-// console.log(firstDivElement);
+let firstDivElement = document.querySelector('div');
+console.log(firstDivElement);
 
-// const jumbotron = document.querySelector('#jumbotron-text');
-// console.log(jumbotron);
+const jumbotron = document.querySelector('#jumbotron-text');
+console.log(jumbotron);
 
-// let primary= document.querySelector('.primary-content');
-// let pElements = primary.querySelectorAll('p');
-// console.log(pElements);
+let primary= document.querySelector('.primary-content');
+let pElements = primary.querySelectorAll('p');
+console.log(pElements);
 
 
 /*
@@ -38,11 +38,11 @@ When a user clicks the 'ALERT' button, an alert box should pop up
 with the text "Thanks for visiting Bikes for Refugees!"
 */
 
-// document.getElementById('alertBtn').addEventListener('click', function(){
+document.getElementById('alertBtn').addEventListener('click', function(){
     
-//     alert('"Thanks for visiting Bikes for Refugees!"');
+    alert('"Thanks for visiting Bikes for Refugees!"');
 
-// });
+});
 
 
 // --------------2ND WAY-------
@@ -60,12 +60,12 @@ Task 3
 
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
-    // let changeColor = document.querySelector('#bgrChangeBtn');
+    let changeColor = document.querySelector('#bgrChangeBtn');
 
-    // changeColor.addEventListener('click', function () {
-    //     document.body.style.backgroundColor = 'green';
+    changeColor.addEventListener('click', function () {
+        document.body.style.backgroundColor = 'green';
 
-    // });
+    });
 
 /*
 Task 4
@@ -75,14 +75,14 @@ When a user clicks the ‘Add some text’ button, a new paragraph should
 be added inside the section that says “LEARN MORE”
 */
 
-//  document.getElementById('addTextBtn').addEventListener('click', function(){
+ document.getElementById('addTextBtn').addEventListener('click', function(){
     
-// var node = document.createElement("P");  
-// var textNode = document.createTextNode('“LEARN MORE”') ;
-//  node.appendChild(textNode);  // p<>LEARN MORE
-//  return document.querySelector('.buttons').appendChild(node);   
+var node = document.createElement("P");  //p tag created 
+var textNode = document.createTextNode('“LEARN MORE”') ; // text added 
+ node.appendChild(textNode);  // p<>LEARN MORE
+ return document.querySelector('.buttons').appendChild(node);   
 
-// });
+});
 
 // --------------------------------------------------------
 
@@ -111,22 +111,13 @@ document.getElementById('largerLinksBtn').addEventListener('click', myFunc);
 
 function myFunc(){
 
- var x = document.querySelectorAll("a");
-   x.forEach((tag) => {
+ var selectAllTags = document.querySelectorAll("a");
+ selectAllTags.forEach((tag) => {
        tag.style.fontSize = "xx-large"
    });
-//    document.getElementsByTagName("a");
     
     
 }
-
-
-// document.getElementById('largerLinksBtn').addEventListener('click', function(){
-//    var span = document.querySelectorAll('a');
-//    span.style.fontSize = 'xx-large';
-//    // document.body.a.style.fontSize = '30px';
-// })
-
 
 
 
@@ -138,17 +129,22 @@ Using the same function in Task 4,
 When the 'Add' button is clicked, get the text inside the input field and create 
 a new paragraph in the "LEARN MORE" section. Also clear the text inside the input field
 */
-// document.getElementById("addArticleBtn").addEventListener('click', newParagraph);
 
-// function newParagraph(){
-//     let tag = document.createElement('P');
-//     let text = document.createTextNode('This is a new a paragraph !!');
-    
-//     tag.appendChild(text);
-//     let targetDiv = document.getElementById('mainArticles');
-//     let inputAtFirst = targetDiv.insertBefore(tag, targetDiv.childNodes[0]);
-//     inputAtFirst.style.color = 'red';
-// }
+
+
+
+
+var targetDiv = document.getElementById('addArticleBtn').addEventListener('click', newParagraph);
+function newParagraph(){
+    var newTag = document.createElement('P');
+    var newText = document.createTextNode('This is a new a paragraph !!');
+    newTag.appendChild(newText);
+    console.log(newTag);
+
+var inputAtFirst = document.getElementById('mainArticles');
+  let result = inputAtFirst.insertBefore(newTag, inputAtFirst.childNodes[1]);
+result.style.color = 'red';
+}
 
 /*
 Task 7
@@ -162,23 +158,23 @@ The next color when you are in the last color of the array will be the first col
 
 
 
-// document.getElementById('bgrChangeBtn').addEventListener('click', function(){
+document.getElementById('bgrChangeBtn').addEventListener('click', function(){
 
-//     var arrColors = ['red', 'green', 'blue', 'black', 'yellow'];
-//     var randomColor =   Math.floor(Math.random() * arrColors.length);
+    var arrColors = ['red', 'green', 'blue', 'black', 'yellow'];
+    var randomColor =   Math.floor(Math.random() * arrColors.length);
 
         
-//      var result = arrColors[randomColor];
+     var result = arrColors[randomColor];
 
-//      console.log()
-
-//     //  var finalresult = "'" + result + "'";
-//      document.body.style.backgroundColor = result ;
-//      console.log(result);
-    //  console.log(finalresult);
+     console.log()
 
    
-// });
+     document.body.style.backgroundColor = result ;
+     console.log(result);
+    
+
+   
+});
 
 // var randomize = Math.floor(Math.random()*myColors.length);
 
--- week-1/Homework/dom-manipulation-2/js/main.js
+++ week-1/Homework/dom-manipulation-2/js/main.js
@@ -1,2 +1,7 @@
-let a = document.querySelector('.jumbotron');
+var a = document.querySelector('.jumbotron');
 console.log(a);
+
+blueBtn.addEventListener('click', () => {
+    a.style.backgroundColor = "#588fbd"
+})
+
